734fa463142c59d7c14d3446180b32cd974a2188
maniginam <maniginam@gmail.com>
2020-12-22 17:35:49 -0600

    server connected

---
+++ spec/webttt/spec_helper.clj
@@ -0,0 +1,5 @@
+(ns webttt.spec-helper
+  (:require [speclj.core :refer :all]))
+
+(defn run-server[& options]
+  )
---
+++ src/webttt/starter.clj
@@ -0,0 +1,23 @@
+(ns webttt.starter
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.java.shell :as shell]))
+
+(defn config []
+  (let [file (str (.getCanonicalPath (io/file ".")) "/config.edn")
+      config (read-string (slurp file))]
+    config
+  ))
+
+(defn start-server [config & options]
+    (apply shell/sh (concat (str/split config #" ") options)))
+
+(defn start-ttt [config & options]
+  (println "options: " options)
+  (println "cmd: " config)
+  (apply shell/sh (concat (str/split config #" ") options)))
+
+(defn -main [& options]
+  (let [config (config)]
+  (start-server (:server config) options)
+  (start-ttt (:ttt config) options)))
